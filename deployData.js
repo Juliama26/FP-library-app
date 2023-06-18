2e77d6796bebe13623af40d7d777b7834e4a955b
Juliama26 <juliamalaoli26@gmail.com>
2023-06-18 11:41:05 +0700

    FP-library-app

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Venansia Juliama Laoli"; // TODO: replace this
+
+const studentId = "FE4549548"; // TODO: replace this
+
+const NetlifyDeployUrl = "https://fp-library-app.netlify.app/"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
